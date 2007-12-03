dd6495fa7d8cb54508c62ae480d458e682f966e7
flori <flori@f11af0ed-4408-0410-895c-f1525b0d0c21>
2007-12-03 08:05:57 +0000

    git-svn-id: svn://gate.dyna.ping.de/jsturtle/trunk@4052 f11af0ed-4408-0410-895c-f1525b0d0c21

--- turtle.js
+++ turtle.js
@@ -1,15 +1,19 @@
 var Turtle = {
-  drawing:  true,
+  drawing:  false,
+  hidden:   false,
   angle:    0,
-  posX: 0,
-  posY: 0,
+  posX:     0,
+  posY:     0,
   canvasId: null,
   canvas:   null,
   ctx:      null,
-  angleAsRad: function(angle) { return Turtle.angle * Math.PI / 180; }
 };
 
-function initCanvas(canvasId, bgcolor, pcolor) {
+Turtle.angleAsRad = function(angle) {
+  return Turtle.angle * Math.PI / 180;
+};
+
+Turtle.init = function(canvasId, bgcolor, pcolor) {
   var canvas = document.getElementById(canvasId);
   if (!canvas.getContext) return;
   Turtle.canvasId = canvasId;
@@ -22,8 +26,60 @@ function initCanvas(canvasId, bgcolor, pcolor) {
   if (!pcolor) pcolor = 'white';
   setPenColor(pcolor);
 
+  clearScreen();
+
   return true;
+};
+
+Turtle.moveTo = function(x, y) {
+  if (Turtle.drawing) {
+    Turtle.ctx.moveTo(Turtle.posX, Turtle.posY);
+    Turtle.ctx.lineTo(Turtle.posX + x, Turtle.posY + y);
+    Turtle.ctx.stroke();
+  }
+  Turtle.posX += x;
+  Turtle.posY += y;
+};
+
+Turtle.forward = function(length, turtle) {
+  var x = length * Math.cos(Turtle.angleAsRad());
+  var y = length * Math.sin(Turtle.angleAsRad());
+  Turtle.moveTo(x, y);
+  if (turtle) Turtle.drawTurtle();
+};
+
+Turtle.back = function(length, turtle) {
+  var x = length * Math.cos(Turtle.angleAsRad() - Math.PI);
+  var y = length * Math.sin(Turtle.angleAsRad() - Math.PI);
+  Turtle.moveTo(x, y);
+  if (turtle) Turtle.drawTurtle();
+};
+
+Turtle.rotate = function(addAngle, turtle) {
+  Turtle.angle += addAngle;
+  Turtle.angle %= 360;
+  if (Turtle.angle < 0) Turtle.angle += 360;
+  if (turtle) Turtle.drawTurtle();
 }
+
+Turtle.drawTurtle = function() {
+  if (Turtle.hidden) return;
+  var oldDrawing = Turtle.drawing;
+  var oldPos = pos();
+  var oldHeading = Turtle.angle;
+  penUp();
+  Turtle.back(10);
+  Turtle.rotate(-90);
+  Turtle.forward(5);
+  penDown();
+  repeat(3, function() {
+    Turtle.rotate(120);
+    Turtle.forward(10);
+  });
+  setPos(oldPos);
+  setHeading(oldHeading);
+  Turtle.drawing = oldDrawing;
+};
 
 function clean() {
   Turtle.ctx.clearRect(0, 0, Turtle.canvas.width, Turtle.canvas.height);
@@ -34,60 +90,81 @@ function clearScreen() {
   Turtle.posX = Turtle.canvas.width / 2;
   Turtle.posY = Turtle.canvas.height / 2;
   Turtle.angle = 0;
+  Turtle.drawing = true;
+  Turtle.hidden = true;
+  Turtle.drawTurtle();
 }
 var cs = clearScreen;
+
+function hideTurtle() {
+  Turtle.hidden = true;
+}
+var ht = hideTurtle;
+
+function isTurtleHidden() {
+  return Turtle.hidden;
+}
+
+function showTurtle() {
+  Turtle.hidden = false;
+}
+var st = showTurtle;
+
+function isTurtleShown() {
+  return !Turtle.hidden;
+}
+
+// wrap, window, fence
+
+// fill
 
 function penUp() {
   Turtle.drawing = false;
 }
 var pu = penUp;
 
+function isPenUp() {
+  return !Turtle.drawing;
+}
+
 function penDown() {
   Turtle.drawing = true;
 }
 var pd = penDown;
 
+function isPenDown() {
+  return Turtle.drawing;
+}
+
+// penPaint, penErase, penRevert
+
 function setPenColor(color) {
   Turtle.ctx.strokeStyle = color;
 }
-setpc = setPenColor;
+setPC = setPenColor;
 
 function setBackground(color) {
   Turtle.canvas.style.background = color;
 }
-setbg = setBackground;
+setBG = setBackground;
+
+function setPenSize(size) {
+  
+}
 
 function forward(length) {
-  var x = length * Math.cos(Turtle.angleAsRad());
-  var y = length * Math.sin(Turtle.angleAsRad());
-  if (Turtle.drawing) {
-    Turtle.ctx.moveTo(Turtle.posX, Turtle.posY);
-    Turtle.ctx.lineTo(Turtle.posX + x, Turtle.posY + y);
-    Turtle.ctx.stroke();
-  }
-  Turtle.posX += x;
-  Turtle.posY += y;
+  Turtle.forward(length, true);
 }
 var fd = forward;
 
 
 function back(length) {
-  var x = length * Math.cos(Turtle.angleAsRad() - Math.PI);
-  var y = length * Math.sin(Turtle.angleAsRad() - Math.PI);
-  if (Turtle.drawing) {
-    Turtle.ctx.moveTo(Turtle.posX, Turtle.posY);
-    Turtle.ctx.lineTo(Turtle.posX + x, Turtle.posY + y);
-    Turtle.ctx.stroke();
-  }
-  Turtle.posX += x;
-  Turtle.posY += y;
+  Turtle.back(length, true);
 }
 var bk = back;
 
 function rotate(addAngle) {
-  Turtle.angle += addAngle;
-  Turtle.angle %= 360;
-  if (Turtle.angle < 0) Turtle.angle += 360;
+  Turtle.rotate(addAngle, true);
 }
 
 function setHeading(angle) {
@@ -162,19 +239,39 @@ function setY(y) {
   Turtle.posY = y;
 }
 
-function squares() {
-  home();
-  var n = 0;
-  var s = 20;
-  while (n <= 360) {
-    forward(100);
-    right(90);
-    forward(100);
-    right(90);
-    forward(100);
-    right(90);
-    forward(100);
-    right(90 + s);
-    n += s;
-  }
+function repeat(n, block) {
+  for (var i = 0; i < n; i++) block(i);
 }
+
+function forever(block) {
+  var i = 0;
+  for (;; i++) block(i);
+}
+
+function regPoly(edges, size) {
+  repeat(edges, function() {
+    forward(size);
+    right(360 / edges);
+  });
+}
+
+function squares() {
+  var size = 10;
+  var step = 10;
+  penUp();
+  back(100);
+  right(90);
+  forward(100);
+  left(180);
+  penDown();
+  repeat(36, function() {
+    regPoly(7, size);
+    right(step);
+    size *= 1.08;
+  });
+}
+
+function draw() {
+  squares();
+}
+
